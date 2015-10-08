6fd9e1b603c641fc5091669cbca7e194918fc1df
afbuck293 <abuck@smu.edu>
2015-10-07 19:49:00 -0500

    Successfully read in and modulate/demodulate voice file.

--- Source/matlab_code/ReadVoiceFile.m
+++ Source/matlab_code/ReadVoiceFile.m
@@ -1,14 +1,46 @@
-[m, Fs] = wavread('test file for signals.wav');
+clc
+clear all
+
+[m, Fs] = audioread('test file for signals.wav');
+ts = 1/Fs;
 m = m(:,1);
-length = length(m);
-t = (0: length - 1)/Fs;
-plot(t, m);
-m = fftshift(fft(m, length));
-M_mag = abs(m);
-df = (-length/2 : length/2 -1)*(Fs/length);
-plot(df, M_mag);
+m = m';
+
+Length = length(m);
+t = (0 : Length - 1)/Fs;
 
 
+M = fftshift(fft(m, Length));
+M_sig = abs(M);
+freqm = (-Length/2 : Length/2 -1)/(Length/ts);
+
+
+
+B_m = 5000;
+h=fir1(40,[B_m*ts]);
+
+fc = 40000;
+s_dsb = m.*cos(2*pi*fc*t);
+lfft= length(t);
+lfft = 2^ceil(log2(lfft)+1);
+S_dsb = fftshift(fft(s_dsb,lfft));
+freqs = (-lfft/2:lfft/2-1)/(lfft*ts);
+%am modulation
+
+s_dem = s_dsb.*cos(2*pi*fc*t)*2;
+S_dem=fftshift(fft(s_dem,lfft));
+
+s_rec = filter(h,1,s_dem);
+S_rec = fftshift(fft(s_rec,lfft));
+
+figure, plot(t, m);
+figure, plot(t, s_rec);
+figure, plot(freqm, M_sig);
+figure, plot(freqs, abs(S_rec));
+
+
+
+%{
 fSampling = Fs;
 yt = m;
 fc = 15000;
@@ -23,5 +55,5 @@ y2 = y.*cos(2*pi*fc*t);
 mody2_fft = fft(y2);
 n2 = numel(mody2_fft);
 f2 = 0:fSampling/n2:fSampling*(n2-1)/n2;
-plot(f2, abs(mody2_fft))
-
+figure, plot(f2, abs(mody2_fft))
+%}
